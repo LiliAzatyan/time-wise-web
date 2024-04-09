be98c85b32316cce2ca1b8b30c9bb0f0b04004f1
Lily <liliazatyan2000@gmail.com>
2024-04-09 19:11:37 +0400

    new project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import { HOME, LOGIN, SOCIAL, PLANNER, INVITE } from "./components/constants/path";
+import { LOGIN } from "./components/constants/path";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginAndRegister from "./pages/login-and-register";
-import SocialMedia from "./pages/social-media";
-import TimePlanner from "./pages/time-planner";
-import Header from "./layouts/header";
-import HomePage from "./pages/home";
-import Invite from "./pages/invite";
 import React from "react";
 import "./App.css";
-import LeftSide from "./components/LeftSide/LeftSide";
 
 const App = () => {
   
   return (
     
     <BrowserRouter>
-      <LeftSide/>
-      <Header />
       <Routes>
         <Route path={LOGIN} element={<LoginAndRegister />} />
-        <Route path={HOME} element={<HomePage />} />
-        <Route path={SOCIAL} element={<SocialMedia />} />
-        <Route path={PLANNER} element={<TimePlanner />} />
-        <Route path={INVITE} element={<Invite />} />
       </Routes>
     </BrowserRouter>
   );
