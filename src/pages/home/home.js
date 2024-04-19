7c2ae6eaa11944a47f649f1bc4d2ba10e8e80c14
VaheKhachtrian <vahe.khachtryan.0806@gmail.com>
2024-04-19 14:14:21 +0400

    commit

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-scroll";
 import "./home.css";
+import { Button, Modal } from "antd";
+import Login from "../login-and-register/login/index";
+import Registration from "../login-and-register/register/index";
+
 import image from "./Photos/image.jpg";
 import image1 from "./Photos/image1.png";
 import image2 from "./Photos/image2.png";
@@ -12,12 +16,83 @@ function Home() {
     setTimeout(() => setIsClicked(false), 1000);
   };
 
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const [isRegistrationModalOpen, setIsRegistrationModalOpen] = useState(false);
+  const [isBackgroundBlurred, setIsBackgroundBlurred] = useState(false);
+
+  const showLoginModal = () => {
+    setIsLoginModalOpen(true);
+    setIsBackgroundBlurred(true); // Apply blur effect to the background
+  };
+
+  const showRegistrationModal = () => {
+    setIsRegistrationModalOpen(true);
+    setIsBackgroundBlurred(true); // Apply blur effect to the background
+  };
+
+  const handleLoginOk = () => {
+    setIsLoginModalOpen(false);
+    setIsBackgroundBlurred(false); // Remove blur effect from the background
+  };
+
+  const handleLoginCancel = () => {
+    setIsLoginModalOpen(false);
+    setIsBackgroundBlurred(false); // Remove blur effect from the background
+  };
+
+  const handleRegistrationOk = () => {
+    setIsRegistrationModalOpen(false);
+    setIsBackgroundBlurred(false); // Remove blur effect from the background
+  };
+
+  const handleRegistrationCancel = () => {
+    setIsRegistrationModalOpen(false);
+    setIsBackgroundBlurred(false); // Remove blur effect from the background
+  };
+
   return (
-    <div>
+    <div className={`blur-container ${isBackgroundBlurred ? 'blur-background' : ''}`}>
       <div className="header">
         <h1 className="typing-demo">
           Organize your life with our Time Wise app
         </h1>
+        <Button type="default" onClick={showLoginModal}>
+          Sign in
+        </Button>
+
+        <Button type="default" onClick={showRegistrationModal}>
+          Sign up
+        </Button>
+
+        {isLoginModalOpen && (
+          <div className="modal-container">
+            <Modal
+              title="Login"
+              visible={isLoginModalOpen}
+              onOk={handleLoginOk}
+              onCancel={handleLoginCancel}
+              style={{ width: "100%" }}
+              footer={null}
+            >
+              <Login />
+            </Modal>
+          </div>
+        )}
+
+        {isRegistrationModalOpen && (
+          <div className="modal-container">
+            <Modal
+              title="Registration"
+              visible={isRegistrationModalOpen}
+              onOk={handleRegistrationOk}
+              onCancel={handleRegistrationCancel}
+              style={{ width: "100%" }}
+              footer={null}
+            >
+              <Registration />
+            </Modal>
+          </div>
+        )}
       </div>
       <Link
         activeClass="active"
@@ -26,8 +101,7 @@ function Home() {
         smooth={true}
         offset={-70}
         duration={500}
-      >
-      </Link>
+      ></Link>
       <Link
         activeClass="active"
         to="section2"
@@ -35,8 +109,7 @@ function Home() {
         smooth={true}
         offset={-70}
         duration={500}
-      >
-      </Link>
+      ></Link>
       <Link
         activeClass="active"
         to="section3"
@@ -44,8 +117,7 @@ function Home() {
         smooth={true}
         offset={-70}
         duration={500}
-      >
-      </Link>
+      ></Link>
       <Link
         activeClass="active"
         to="section4"
@@ -53,25 +125,24 @@ function Home() {
         smooth={true}
         offset={-70}
         duration={500}
-      >
-      </Link>
+      ></Link>
       <div id="section1" className="section">
         <div className="row">
           <div className="text">
             <h2>Time Wise: Organize work schedules with ease</h2>
             <p>
-            Time Wise is a user-friendly app for organizing work
-              schedules, meetings, tasks, and documents efficiently.
+              Time Wise is a user-friendly app for organizing work schedules,
+              meetings, tasks, and documents efficiently.
             </p>
 
-                <a href="/my-planner" onClick={handleClick}>
+            <a href="/my-planner" onClick={handleClick}>
               <button
                 className={`button ${isClicked ? "bubble" : ""}`}
                 onClick={handleClick}
               >
                 Start Planning Now
               </button>
-                </a>
+            </a>
           </div>
           <div className="images">
             <img src={image} alt="Image" />
@@ -89,12 +160,12 @@ function Home() {
           </div>
           <div className="text">
             <h2>
-            Time Wise offers seamless task and document management with
+              Time Wise offers seamless task and document management with
               calendar integration
             </h2>
             <p>
-              Stay organized with Time Wise's user-friendly interface,
-              task management, and calendar integration for efficient work
+              Stay organized with Time Wise's user-friendly interface, task
+              management, and calendar integration for efficient work
               scheduling.
             </p>
 
@@ -102,9 +173,10 @@ function Home() {
               <button
                 className={`button ${isClicked ? "bubble" : ""}`}
                 onClick={handleClick}
-              >Start organizing now</button>
+              >
+                Start organizing now
+              </button>
             </a>
-
           </div>
         </div>
       </div>
@@ -121,8 +193,8 @@ function Home() {
         <div className="row">
           <div className="text">
             <h2>
-              Boost productivity with Time Wise's task management and
-              calendar integration.
+              Boost productivity with Time Wise's task management and calendar
+              integration.
             </h2>
             <p>
               Time Wise enhances work efficiency with task management and
@@ -132,7 +204,9 @@ function Home() {
               <button
                 className={`button ${isClicked ? "bubble" : ""}`}
                 onClick={handleClick}
-              >Discover more features</button>
+              >
+                Discover more features
+              </button>
             </a>
           </div>
           <div className="images">
@@ -159,11 +233,14 @@ function Home() {
             </h2>
           </div>
           <div className="text">
-          <a href="/goal-tracking" onClick={handleClick}>
-              
-            <button className={`add-goals-button ${isClicked ? "bubble" : ""}`}
-                onClick={handleClick}>Add your goals</button>
-          </a>
+            <a href="/goal-tracking" onClick={handleClick}>
+              <button
+                className={`add-goals-button ${isClicked ? "bubble" : ""}`}
+                onClick={handleClick}
+              >
+                Add your goals
+              </button>
+            </a>
           </div>
         </div>
       </div>
