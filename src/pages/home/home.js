c18d9d127714a38399fbdb9754f4e7c7304601f6
Lily <liliazatyan2000@gmail.com>
2024-05-17 13:43:21 +0400

    home page is changed

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,11 @@
 import { Link } from "react-scroll";
 import React from "react";
 import "./home.css";
+import Lottie from "lottie-react";
+import animationHeading from "../../animations/heading-animation.json";
+import plannerAnimation from "../../animations/planner-animation.json";
+import goalAnimation from "../../animations/goal-animation.json";
+import notesAnimation from "../../animations/notes-animation.json";
 
 function Home() {
   return (
@@ -110,9 +115,10 @@ function Home() {
               </button>
             </div>
           </div>
-          {/* <div className="background-image"> */}
-            <img src="Hedaer__bg.png" alt="Background" className="background-image-header"/>
-          {/* </div> */}
+          <div className="background-image-header">
+            {/* <img src="Hedaer__bg.png" alt="Background" className="background-image-header"/> */}
+            <Lottie animationData={animationHeading} />
+          </div>
         </div>
       </div>
 
@@ -124,31 +130,75 @@ function Home() {
         <div className="text">
           <h2 className="common-heading">My Planner</h2>
           <p className="common-text">
-            A my planner helps you organize your day with sections for date and
-            time, task lists, prioritization, notes, and sometimes goal setting
-            and reflection. It's portable and keeps you focused and productive
-            by outlining your schedule and priorities.
+            My Planner page is designed to help you structure your days, weeks,
+            and months with ease. Whether you're juggling personal tasks or
+            professional deadlines, My Planner offers intuitive features to:{" "}
+            <br />
+            <ul style={{ marginLeft: "50px" }}>
+              <li>
+                Organize Your Tasks: Easily list and categorize your tasks to
+                see what needs to be done at a glance.
+              </li>
+              <li>
+                Set Priorities: Highlight important tasks to ensure you focus on
+                what matters most.
+              </li>
+              <li>
+                Track Progress: Monitor your progress and stay motivated as you
+                complete your tasks.
+              </li>
+              <li>
+                Stay Flexible: Adjust your plans as needed to accommodate
+                changes and new priorities.
+              </li>
+            </ul>
+            With My Planner, you'll stay productive, balanced, and in control of
+            your schedule. Plan your time effectively, reduce stress, and
+            achieve your goals with confidence. Your time, your plan, your
+            success.
           </p>
         </div>
         <div className="images">
-          <img src="image1.png" alt="my planner" width="500px"/>
+          {/* <img src="image1.png" alt="my planner" width="500px"/> */}
+          <Lottie animationData={plannerAnimation} />
         </div>
       </div>
       <div
         className="section3_header section"
         style={{ backgroundColor: "rgba(123, 214, 212, 0.15)" }}
       >
-        <div className="image">
-          <img src="image2.png" alt="aa" />
+        <div className="goal-animation">
+          {/* <img src="image2.png" alt="aa" /> */}
+          <Lottie animationData={goalAnimation} />
         </div>
 
         <div className="text">
           <h2 className="common-heading">My Goals</h2>
           <p className="common-text">
-            Your Goals Web Page: Your digital roadmap for growth. Set SMART
-            goals - Specific, Measurable, Achievable, Relevant, Time-bound. Keep
-            track, stay aligned, and adjust as needed. Your journey, your pace,
-            your success.
+            My Goals page empowers you to set, track, and achieve your
+            aspirations with precision. Here, you can:
+            <ul style={{ marginLeft: "50px" }}>
+              <li>
+                Set Meaningful Goals: Define your ambitions clearly and
+                concisely, ensuring they resonate with your values and desires.
+              </li>
+              <li>
+                Break Down Goals: Deconstruct larger objectives into manageable
+                tasks, making them less daunting and more achievable.
+              </li>
+              <li>
+                Monitor Progress: Keep tabs on your journey towards each goal,
+                celebrating milestones and staying motivated along the way.
+              </li>
+              <li>
+                Stay Accountable: Hold yourself accountable by regularly
+                reviewing your progress and adjusting your strategies as needed.
+              </li>
+            </ul>
+            With My Goals, you have the tools and support you need to turn your
+            dreams into reality. Whether it's advancing your career, cultivating
+            new skills, or enhancing your well-being, this page is your compass
+            for success. Your goals, your journey, your triumphs.
           </p>
         </div>
       </div>
@@ -161,14 +211,22 @@ function Home() {
         <div className="text">
           <h2 className="common-heading">My Notes</h2>
           <p className="common-text">
-            Your Notes Web Page: Your digital notebook for capturing thoughts,
-            ideas, and insights. Organize, reflect, and ideate freely. Your
-            space to jot, brainstorm, and remember. Your thoughts, your
-            creativity, all in one place.
+            My Notes page, your versatile digital companion for capturing
+            thoughts, ideas, and reminders on the fly. Tailored for convenience
+            and efficiency, this platform allows you to categorize your notes
+            effortlessly into distinct sections such as Work, Life, and Study.
+            Whether you're brainstorming project ideas, jotting down personal
+            reflections, or keeping track of study notes, My Notes offers a
+            seamless and organized solution. With easy access anytime, anywhere,
+            and customizable features like tags and labels, you can streamline
+            your note-taking process and stay productive throughout your day.
+            Your thoughts, your categories, your convenience - all in one place
+            with My Notes.
           </p>
         </div>
         <div className="images">
-          <img src="image3.png" alt="My goals" />
+          {/* <img src="image3.png" alt="My goals" /> */}
+          <Lottie animationData={notesAnimation} />
         </div>
       </div>
       <div
